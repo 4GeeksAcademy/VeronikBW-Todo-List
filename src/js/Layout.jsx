56c9f5f07216c4ef6ee24fbdc3723ecc164f2475
Verónica Rodríguez <veronikbw@gmail.com>
2025-09-11 21:59:18 +0000

    previa

---
+++ src/js/Layout.jsx
@@ -0,0 +1,9 @@
+import Home from "./components/Home"
+
+const Layout = () => {
+    return(
+        <Home />
+    )
+}
+
+export default Layout
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,83 @@
-import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	
+
+	const [writtingTodo, setWrittingTodo] = useState({
+		task: "",
+		done: "",
+	})
+
+	const [todo, setTodo] = useState([
+
+	])
+
+	const handleChange = (event) => {
+		setWrittingTodo({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	const handleSubmit = (event) => {
+
+
+		event.preventDefault()
+
+		// if (!writtingTodo) return
+
+
+		setTodo([
+			...todo,
+			writtingTodo,
+		])
+
+		setWrittingTodo({
+			task: "",
+			done: "",
+		})
+
+		
+	}
+
+
+
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="titulo col-12 col-md-7 text-center">
+					<p>todos</p>
+				</div>
+				<div className="col-12 col-md-7 bg-white p-3">
+					<form
+						onSubmit={handleSubmit}
+					>
+						<div className="input-group flex-nowrap">
+							<input
+								type="text"
+								name="task"
+								className="form-control border-0"
+								placeholder="What needs to be done?"
+								value={writtingTodo.task}
+								aria-label="Username"
+								aria-describedby="addon-wrapping"
+								onChange={handleChange}
+							/>
+						</div>
+					</form>
+					<div className="col-12 col-md-7 ps-1 mt-3">{
+						todo.map((item, index) => (
+							<div key={index} className="taskToDo border-bottom d-flex justify-content-between">
+								<div>{item.task}</div>
+								<div className="">
+									<button type="button" className="btn-close delete-btn color-black" aria-label="Close"></button>
+								</div>
+							</div>
+						))
+					}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
